81fa045ea53ea2531d1e0200a160355f69cc20d3
Bahadir Altun <ibahadiraltun@gmail.com>
2025-04-25 11:48:04 -0400

    feat(kafka): implement kafka producer to fetch exchange rates

--- data/exchange_rate_producer.py
+++ data/exchange_rate_producer.py
@@ -1,10 +1,49 @@
 import confluent_kafka.admin as KafkaAdmin
+from confluent_kafka import Producer
+import requests
 
+currencies = [
+    "usd", "eur", "jpy", "gbp", "cny", "aud", "cad", "chf",
+    "hkd", "sgd", "sek", "krw", "nok", "nzd", "inr", "mxn",
+    "twd", "zar", "brl", "dkk", "pln", "thb", "ils", "idr",
+    "czk", "aed", "try", "huf", "clp", "sar", "php", "myr",
+    "cop", "rub", "ron", "pen"
+]
 
-admin_client = KafkaAdmin.AdminClient({
+topic_name = "exchange-rates"
+config = {
     "bootstrap.servers": "localhost:50761"
-})
+}
 
-topic_list = []
-topic_list.append(KafkaAdmin.NewTopic("example_topic2", 1, 1))
-admin_client.create_topics(topic_list)
+
+def callback(err, msg):
+    if err:
+        print('ERROR: Message failed delivery: {}'.format(err))
+    else:
+        print("Produced event to topic {topic}: key = {key} value = {value} partition = {partition}".format(
+            topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8'), partition=msg.partition()))
+
+
+def fetch_rate(currency='usd'):
+    api = 'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/' + f'{currency}.json'
+    return requests.get(api).json()
+
+
+producer = Producer({**config, "acks": "all"})
+idx = 0
+while True:
+    for base_currency in currencies:
+        res = fetch_rate(base_currency)
+        timestamp = res['date']
+        rates = res[base_currency]
+        for target_currency, rate in rates.items():
+            producer.produce(
+                topic=topic_name,
+                value=str(rate),
+                key=f"{base_currency}:{target_currency}@{timestamp}",
+                partition=idx,
+                callback=callback
+            )
+        idx = (idx + 1) % 36
+        producer.poll(1000)
+
